d79a12f9350ecb7b36acbe920da9fe9d0bcc55d3
Hans Orozco <104253434+hansorozco@users.noreply.github.com>
2022-04-27 17:39:29 +0800

    First Commit

---
+++ Message Box 1/Message Box 1/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("Hello, I am Hans Christian A. Orozco,
+                How are you?")
+    End Sub
+End Class
